5b28f738a28c833049ce5c52d814147832408c11
Xiaoxiao Li <xiaoxiao.li@yale.edu>
2021-05-13 03:33:29 -0400

    Update 01-fetch_data.py

--- 01-fetch_data.py
+++ 01-fetch_data.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+'''
+This script mainly refers to https://github.com/kundaMwiza/fMRI-site-adaptation/blob/master/fetch_data.py
+'''
+
 from nilearn import datasets
 import argparse
 from imports import preprocess_data as Reader
